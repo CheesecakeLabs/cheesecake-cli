a75c9a3aeacd260514d47cf741fffd09a014ce63
Paulo Real <paulo.real@ckl.io>
2022-05-30 02:06:46 -0300

    feat: add typography step style

--- src/helpers/typography.sh
+++ src/helpers/typography.sh
@@ -8,6 +8,10 @@ subtitle() {
     printf "\n%b%s%b\n" "$COL_CYAN" "$@" "$COL_RESET"
 }
 
+step() {
+    printf "\n%b%s...%b\n\n" "$COL_GREEN" "$@" "$COL_RESET"
+}
+
 ask() {
     printf "\n%b%s%b " "$COL_GREEN" "$@" "$COL_RESET"
 }
