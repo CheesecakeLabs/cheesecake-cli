37988da1fd6d610da129f0cf8fec45a5babd8bb4
Paulo Real <paulo.real@ckl.io>
2022-05-30 02:17:02 -0300

    feat: check if project was cloned on clone command

--- src/commands/project_clone.sh
+++ src/commands/project_clone.sh
@@ -9,12 +9,16 @@ runCloningRepo() {
         shift 1
     fi
 
-    if [ -d "$PROJECTS_PATH"/"$repository" ]; then
+    if [ -d "${PROJECTS_PATH}/${repository}" ]; then
         exception_message "Error trying to cloning a project with a name that already exists."
     fi
 
     cd "$PROJECTS_PATH" || exception_unexpected
     git clone git@github.com:CheesecakeLabs/"$repository".git
+
+    if [ ! -d "${PROJECTS_PATH}/${repository}" ]; then
+        exception_ssh "$repository"
+    fi
 
     return $?
 }
