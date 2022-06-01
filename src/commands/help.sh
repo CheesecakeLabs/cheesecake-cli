3e2824408a5f420e30c0db38c743ac68456c52dd
Paulo Real <paulo.real@ckl.io>
2022-06-01 16:03:22 -0300

    feat: help command

--- src/commands/help.sh
+++ src/commands/help.sh
@@ -3,12 +3,13 @@
 showHelp() {
     title "HOW TO USE THIS CLI:"
 
-    printf "%bckl%b %b<command>%b <args?>\n" "$COL_LGREEN" "$COL_RESET" "$COL_RED" "$COL_RESET"
-    printf "* parameters with %b< ?> are optionals%b \n\n" "$COL_CYAN" "$COL_RESET"
+    printf "%bckl%b %b<command>%b %b<args?>%b\n" "$COL_LGREEN" "$COL_RESET" "$COL_RED" "$COL_RESET" "$COL_LGRAY" "$COL_RESET"
+    printf "* parameters with %b< ?>%b are optionals \n" "$COL_LGRAY" "$COL_RESET"
     
-    title "BASIC COMMANDS:"
-    printf "%bckl%b %bnew%b                         Create new project \n" "$COL_GREEN" "$COL_RESET" "$COL_RED" "$COL_RESET"
-    printf "%bckl%b %bhelp%b                        Show this help screen \n" "$COL_GREEN" "$COL_RESET" "$COL_RED" "$COL_RESET"
-    
-    printf "\n"
+    title "COMMANDS:"
+    printf "%bckl%b %bnew%b %b<customer?> <project?>%b    Create new project \n" "$COL_GREEN" "$COL_RESET" "$COL_RED" "$COL_RESET" "$COL_LGRAY" "$COL_RESET"
+    printf "%bckl%b %badd%b %b<stack?> <project?>%b       Add a new starter on a project \n" "$COL_GREEN" "$COL_RESET" "$COL_RED" "$COL_RESET" "$COL_LGRAY" "$COL_RESET"
+    printf "%bckl%b %butils install%b %b<util-name?>%b    Install utils \n" "$COL_GREEN" "$COL_RESET" "$COL_RED" "$COL_RESET" "$COL_LGRAY" "$COL_RESET"
+    printf "%bckl%b %bhelp%b                          Show this help screen \n" "$COL_GREEN" "$COL_RESET" "$COL_RED" "$COL_RESET"
+    printf "\n\n"
 }
